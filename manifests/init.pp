da32a811555dcb1d95b4c7e0d01e49b346bf7be3
root <root@atlas.research.uts.edu.au>
2015-02-19 16:33:48 +1100

    Add init/updated plugins back in

---
+++ manifests/init.pp
@@ -0,0 +1,86 @@
+# See README.md for usage
+class omeka (
+  $omeka_ver        = '2.2.2',
+  $omeka_hostname,
+  $mysql_root,
+  $omekadb_user     = 'omeka',
+  $omekadb_password,
+  $omekadb_dbname   = 'omeka_db',
+  $solrsearch       = true,
+  $solr_ver         = '4.10.2'
+) {
+
+  $solr_binary      = "http://mirror.aarnet.edu.au/pub/apache/lucene/solr/${solr_ver}/solr-${solr_ver}.tgz"
+  #$solr_binary      = "http://apache.mirror.digitalpacific.com.au/lucene/solr/${solr_ver}/solr-${solr_ver}.tgz"
+  #$solr_binary      = "http://www.apache.org/dist/lucene/solr/${solr_ver}/solr-${solr_ver}.tgz"
+  $omeka_home       = "/var/www/html/omeka-${omeka_ver}"
+  $omeka_solrconf   = "${omeka_home}/plugins/SolrSearch/solr-core/omeka/conf/solrconfig.xml"
+  
+  class { '::omeka::db':
+    omeka_home       => $omeka_home,
+    mysql_root       => $mysql_root,
+    omekadb_dbname   => $omekadb_dbname,
+    omekadb_user     => $omekadb_user,
+    omekadb_password => $omekadb_password,
+  }
+  
+  class { '::apache': }
+  
+  apache::vhost { $omeka_hostname:
+    port        => '80',
+    docroot     => $omeka_home,
+    directories => [
+      { path           => $omeka_home,
+        allow_override => ['All'],
+      }
+    ]
+  }
+  
+  package { 'ImageMagick':  ensure => installed, }
+  
+  class { 'selinux':
+    mode => 'disabled',
+  }
+  
+  package {'php':
+    ensure => 'installed',
+  }
+  
+  
+  archive { 'omeka-zip':
+    ensure    => 'present',
+    url       => "http://omeka.org/files/omeka-${omeka_ver}.zip",
+    target    => '/var/www/html',
+    extension => 'zip',
+    checksum  => false,
+  }
+
+  
+  package {'curl': ensure => installed }
+  package { 'java-1.7.0-openjdk.x86_64': ensure => 'installed', }
+  
+  class { '::apache::mod::php': }
+  
+  file { [
+    "${omeka_home}/files",
+    "${omeka_home}/files/original",
+    "${omeka_home}/files/fullsize",
+    "${omeka_home}/files/thumbnails",
+    "${omeka_home}/files/square_thumbnails",
+    "${omeka_home}/files/theme_uploads",
+    ]:
+    ensure  => 'directory',
+    owner   => 'apache',
+    require => Archive['omeka-zip'],
+  }
+  
+  if ($solrsearch) {
+    class { '::omeka::solr':
+      solr_binary    => $solr_binary,
+      omeka_solrconf => $omeka_solrconf,
+      omeka_home     => $omeka_home,
+      omeka_ver      => $omeka_ver
+    }
+  }
+
+}
--- manifests/plugins.pp
+++ manifests/plugins.pp
@@ -1,11 +1,27 @@
 # omeka plugins managed here
-class omeka::plugins () {
+class omeka::plugins (
+  $pdfsearch       = true,
+  $fedoraconnector = true,
+  $bagit = true,
+) {
 
   # The plugin system dependencies
 
-  
+  # Fedora connector
+  if ($fedoraconnector) {
+    package { 'php-xml':       ensure => installed }
+  }
+
+  # PDF Search -- 
+  # requires pdftotext, and poppler-utils specifically
+  # and not the one that comes with xpdf
+  if ($pdfsearch) {
+    package { 'poppler-utils': ensure => installed }
+  }
+
+  if ($bagit) {
+    # Install Archive_Tar php module -- on RHEL it's php-pear
+    package { 'php-pear':      ensure => installed }
+  }
+
 }
-#  400  yum install php-xml
-#  696  yum install php-pear
-#  795  yum -y install pdf2text poppler-utils
-#
