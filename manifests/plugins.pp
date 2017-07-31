7b5b9b7d4b9c54e4568830c1dcfbd0cfc5a405d6
Stephen Houser <stephenhouser@gmail.com>
2017-07-31 14:35:52 -0400

    themes again

--- manifests/plugins.pp
+++ manifests/plugins.pp
@@ -1,71 +1,32 @@
 # omeka plugins managed here
 
 define omeka::plugin(
-	$ensure  = 'present',
-  	$version = 'latest',
-	$source  = undef,
+  $ensure  = 'present',
+  $version = 'latest',
+  $source  = undef,
 ) {
-	$apache_docroot  = hiera('apache::docroot', "/var/www/html")
+  $apache_docroot  = hiera('apache::docroot', "/var/www/html")
   $omeka_version   = hiera('omeka::version', "2.5.1")
 
-	$omeka_home      = "${apache_docroot}/omeka-${omeka_version}"
-	$omeka_plugins   = "${omeka_home}/plugins"
+  $omeka_home      = "${apache_docroot}/omeka-${omeka_version}"
+  $omeka_plugins   = "${omeka_home}/plugins"
 
-	if $source == undef {
-		$url = "http://omeka.org/wordpress/wp-content/uploads/${name}-${version}.zip"
-	} else {
-		$url = $source
-	}
-
-	archive { "${omeka_plugins}/${name}.zip":
-		ensure       => $ensure,
-		source       => $url,
-		extract_path => $omeka_plugins,
-		extract      => true,
-		creates      => "${omeka_themes}/${name}/plugin.ini",
-		cleanup      => true,
-	}  
-
-#	if $source == undef {
-#		archive { "${omeka_plugins}/${plugin}.zip":
-#			ensure       => $ensure,
-#			source       => "http://omeka.org/wordpress/wp-content/uploads/${plugin}-${version}.zip",
-#			extract_path => $omeka_plugins,
-#			extract      => true,
-#			creates      => "${omeka_plugins}/${plugin}/plugin.ini",
-#			cleanup      => true,
-#		}  
-#	} else {
-#		$plugin_dir = "${omeka_plugins}/${plugin}"
-#
-#		file { $plugin_dir:
-#    		ensure  => 'directory',
-#		}
-#
-#		archive { "${omeka_plugins}/${plugin}.zip":
-#			ensure       => $ensure,
-#			source       => $source,
-#			extract_path => $plugin_dir,
-#			extract      => true,
-#			creates      => "${plugin_dir}/plugin.ini",
-#			cleanup      => true,
-#		}  
-#	}
-}
-
-class omeka::plugins(
-  $plugins,
-) {
-  $pdfsearch       = false
-
-  # The plugin system dependencies
-
-  # PDF Search -- 
-  # requires pdftotext, and poppler-utils specifically
-  # and not the one that comes with xpdf
-  if ($pdfsearch) {
-    package { 'poppler-utils': ensure => installed }
+  if $source == undef {
+    $url = "http://omeka.org/wordpress/wp-content/uploads/${name}-${version}.zip"
+  } else {
+    $url = $source
   }
 
+  archive { "${omeka_plugins}/${name}.zip":
+    ensure       => $ensure,
+    source       => $url,
+    extract_path => $omeka_plugins,
+    extract      => true,
+    creates      => "${omeka_themes}/${name}/plugin.ini",
+    cleanup      => true,
+  }  
+}
+
+class omeka::plugins($plugins) {
   create_resources('omeka::plugin', $plugins)
 }
--- manifests/themes.pp
+++ manifests/themes.pp
@@ -1,35 +1,30 @@
 # omeka themes managed here
 
 define omeka::theme(
-	$ensure  = 'present',
-  	$version = 'latest',
-	$source  = undef,
+  $ensure  = 'present',
+  $version = 'latest',
+  $source  = undef,
 ) {
-	$apache_docroot  = hiera('apache::docroot', "/var/www/html")
-  	$omeka_version   = hiera('omeka::version', "2.5.1")
-
-	$omeka_home      = "${apache_docroot}/omeka-${omeka_version}"
-	$omeka_themes    = "${omeka_home}/themes"
-
-	if $source == undef {
-		$url = "http://omeka.org/wordpress/wp-content/uploads/${name}-${version}.zip"
-	} else {
-		$url = $source
-	}
-
-	archive { "${omeka_themes}/${name}.zip":
-		ensure       => $ensure,
-		source       => $url,
-		extract_path => $omeka_themes,
-		extract      => true,
-		creates      => "${omeka_themes}/${name}/theme.ini",
-		cleanup      => true,
-	}  
+  $apache_docroot  = hiera('apache::docroot', "/var/www/html")
+  $omeka_version   = hiera('omeka::version', "2.5.1")
+  $omeka_home      = "${apache_docroot}/omeka-${omeka_version}"
+  $omeka_themes    = "${omeka_home}/themes"
+  if $source == undef {
+  	$url = "http://omeka.org/wordpress/wp-content/uploads/${name}-${version}.zip"
+  } else {
+  	$url = $source
+  }
+  archive { "${omeka_themes}/${name}.zip":
+  	ensure       => $ensure,
+  	source       => $url,
+  	extract_path => $omeka_themes,
+  	extract      => true,
+  	creates      => "${omeka_themes}/${name}/theme.ini",
+  	cleanup      => true,
+  }  
 }
 
-class omeka::themes(
-  $themes,
-) {
+class omeka::themes($themes) {
   create_resources('omeka::theme', $themes)
 }
 
